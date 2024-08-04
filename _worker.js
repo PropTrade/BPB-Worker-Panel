8e6e3965b4e23012830164863b0d4cb0faee1537
Kedaya <97270040+aunsnn-github@users.noreply.github.com>
2024-08-04 19:39:12 +0800

    UUID修改

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '6d98edc8-4ce5-4b7b-b04f-13d5161b988b';//new uuid
+let userID = '3294c072-0be5-4e20-9e2e-d5626d807c9a';//new uuid：3294c072-0be5-4e20-9e2e-d5626d807c9a
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
